ce79152c897f4fd180479e5d9e464ae3b34143f2
haniazipser <90138801+haniazipser@users.noreply.github.com>
2025-04-15 22:50:28 +0200

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -47,26 +47,22 @@ using namespace std;
         //kopiujemy liste
       BRANCH_CLASS* galaz = doSkopiowania.pierwszaGalaz;
       if (galaz != NULL){//jest co kopiowac
-         // cout<<"wywolanie na wys"<< (*galaz).getHeight()<<endl;
+         
           BRANCH_CLASS* kopia = new BRANCH_CLASS(*galaz, this, (*galaz).getHeight());
           (*this).pierwszaGalaz = kopia;
-         // cout<<"Pierwszy:";
+         
           galaz = (*galaz).getNext();
 
           BRANCH_CLASS* poprzedni = kopia;
           while (galaz!=NULL){
-             //cout<<"wywolanie na wys"<< (*galaz).getHeight()<<endl;
+             
             kopia = new BRANCH_CLASS(*galaz, this, getHeight());
             (*poprzedni).setNext(kopia);
-            //cout<<"poprzedni :"<<endl;
-            //(*poprzedni).displayAll();
-            //cout<<"polaczony z :"<<endl;
-            //(*kopia).displayAll();
+            
             poprzedni = kopia;
             galaz = (*galaz).getNext();
           }
-          //cout<<"ostatni:"<<endl;
-          //(*(*pierwszaGalaz).getNext()).displayAll();
+         
       }else{
         (*this).pierwszaGalaz = NULL;
       }
@@ -79,14 +75,14 @@ using namespace std;
         }
         (*this).id = doSkopiowania.id;
         (*this).wysokosc = doSkopiowania.wysokosc;
-        (*this).ileGalezi = 0;//doSkopiowania.ileGalezi;
-        (*this).ileOwocow = 0;//doSkopiowania.ileOwocow;
-        (*this).wagaOwocow = 0;//doSkopiowania.wagaOwocow;
+        (*this).ileGalezi = 0;
+        (*this).ileOwocow = 0;
+        (*this).wagaOwocow = 0;
         next = NULL;
         prev = NULL;
 
       BRANCH_CLASS* galaz = doSkopiowania.pierwszaGalaz;
-      if (galaz != NULL){//jest co kopiowac
+      if (galaz != NULL){
           BRANCH_CLASS* kopia = new BRANCH_CLASS(*galaz, this);
          (*this).pierwszaGalaz = kopia;
           galaz = (*galaz).getNext();
@@ -143,7 +139,7 @@ using namespace std;
     void  TREE_CLASS::growthTree(){
         wysokosc++;
         BRANCH_CLASS* galaz = pierwszaGalaz;
-        if (galaz != NULL){//nie pierwsze
+        if (galaz != NULL){
             while ((*galaz).getNext() != NULL){
                 (*galaz).growthBranch();
                 galaz = (*galaz).getNext();
@@ -218,8 +214,8 @@ using namespace std;
           }
      }
 
-     ///TO DO CLONE BRANCH
-     void TREE_CLASS::cloneBranch(BRANCH_CLASS* doSkopiowania){//pierwszy/ostatni
+     
+     void TREE_CLASS::cloneBranch(BRANCH_CLASS* doSkopiowania){
          BRANCH_CLASS* galaz = pierwszaGalaz;
          BRANCH_CLASS* poprzedni;
          if (galaz != NULL){
@@ -238,8 +234,7 @@ using namespace std;
                 if (galaz != NULL){//mamy dlg = 0
                     BRANCH_CLASS* kopia = new BRANCH_CLASS (*doSkopiowania, this, (*galaz).getHeight());
                     (*poprzedni).setNext(kopia);
-                   // cout<<"poprzedni:"<<(*poprzedni).getHeight()<<endl;
-                    //cout<<"podloczony na next:"<<(*(*poprzedni).getNext()).getHeight()<<endl;
+                   
                     (*kopia).setNext((*galaz).getNext());
                     delete galaz;
                 }
@@ -297,33 +292,6 @@ using namespace std;
 
      void TREE_CLASS::setParent(GARDEN_CLASS* ogrod){
         (*this).ogrod = ogrod;
-     }
-
-     void TREE_CLASS::displayAll(){
-        /*
-        cout<<"D"<<getNumber()<<endl;
-        cout<<" ilosc galezi:"<<ileGalezi<<endl;
-        cout<<" ilosc owocow:"<<ileOwocow<<endl;
-        cout<<" waga owocow:"<<wagaOwocow<<endl;
-        BRANCH_CLASS* galaz = pierwszaGalaz;
-        while (galaz != NULL){
-            (*galaz).wyswietlGalaz();
-            galaz= (*galaz).getNext();
-        }
-        cout<<endl;*/
-        std::cout << ">>fruit_count:" << getFruitsTotal() << " ";
-    std::cout << "branch_count:" << getBranchesTotal() << " ";
-    std::cout << "weights:" << getWeightsTotal() << std::endl;
-    std::cout << "height:" << wysokosc << std::endl;
-    int max = getBranchesTotal(), i = 3;
-    if (max>0) std::cout << ">>branches: \n";
-    while (i <= 3*max){
-        BRANCH_CLASS * branch = getBranchPointer(i);
-        if (branch != NULL) {
-            std:: cout << "-----branch at " << i << std::endl;
-            branch->displayAll();}
-        i+=3;
-    }
      }
 
 
